b52eeb3445af94cbb0209c47690211350e7c5840
Mateus Paixão <mateus.borges.paixao@gmail.com>
2023-04-05 20:41:01 -0300

    first commit :tada:

---
+++ script.js
@@ -0,0 +1,18 @@
+// Tenho uma lista de contatos
+// Preciso mostrar esses contatos em tela 
+// Preciso filtrar esses contatos pelo nome e mostrar em tela 
+// Preciso adicionar uma informação em alguns contatos 
+// Preciso filtrar apenas os contatos que possuem a nova informação 
+
+const contacts = [
+    { id: 1, name: 'Mateus', email: 'mateus@email.com' },
+    { id: 2, name: 'Gustavo', email: 'gustavo@email.com' },
+    { id: 3, name: 'Beatriz ', email: 'beatriz@email.com' },
+    { id: 4, name: 'Marcos', email: 'marcos@email.com' },
+    { id: 5, name: 'Monalisa', email: 'monalisa@email.com' },
+    { id: 6, name: 'João', email: 'jao@email.com' },
+    { id: 7, name: 'Amanda', email: 'amanda@email.com' },
+    { id: 8, name: 'Douglas', email: 'douglas@email.com' },
+    { id: 9, name: 'Sonia', email: 'sonia@email.com' },
+    { id: 10, name: 'Maria', email: 'maria@email.com' },
+]
